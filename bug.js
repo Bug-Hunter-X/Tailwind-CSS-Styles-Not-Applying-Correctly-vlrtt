25fb422a1f64582e3db49d2a0859d9c946cda551
BugHunter-X <askzenai@gmail.com>
2025-02-21 05:34:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Example showing incorrect Tailwind directives
+<div class="bg-red-500 p-4 text-white">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Click Me
+  </button>
+</div>
+```
